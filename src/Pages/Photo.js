a86a563d527c56fee193fbfeae47f8bb83f74d75
Jimmy-Yu-git <77708964+Jimmy-Yu-git@users.noreply.github.com>
2022-01-03 17:06:27 +0800

    0103

--- src/Pages/Photo.js
+++ src/Pages/Photo.js
@@ -1,16 +1,73 @@
+import React, { useState, useCallback } from 'react';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+
+
 const Photo = () => {
+    const [currentImage, setCurrentImage] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [page, setPage] = React.useState(1);
+    const images = [
+        'http://placeimg.com/1200/800/nature',
+        "https://i.ibb.co/nPcy5H7/S-86638597.jpg",
+        'https://i.ibb.co/3NkXv8P/LINE-ALBUM-202213-220103-0.jpg',
+        'https://i.ibb.co/JmjgBXW/LINE-ALBUM-202213-220103-1.jpg',
+        'https://i.ibb.co/R67nZkm/LINE-ALBUM-202213-220103-2.jpg',
+        "https://i.ibb.co/KrF8f6C/image.jpg",
+
+    ];
+    const active = [
+        "",
+        'Xmas party',
+        "Year-End Party",
+        'Year-End Party',
+        'Year-End Party',
+        'Year-End Party',
+
+    ];
+    const handleChange = (event, value) => {
+        setPage(value);
+        console.log(value)
+        // setCurrentImage(images[page])
+        console.log(images[0])
+        console.log(currentImage)
+    };
+    console.log(loaded)
     return (
         <section id="Activity">
             <div class="projects container">
                 <div class="projects-header">
                     <h1 class="section-title"> Activities <span></span></h1>
                 </div>
+                <div class="projects-header">
+                    <h2 class="section-title">{active[`${page}`]}</h2>
+                </div>
                 <div class="all-Photo">
                     <div class="project-item">
                         <div class="Photo-img">
-                            <img src="https://i.ibb.co/nPcy5H7/S-86638597.jpg"alt="img" />
+                            {loaded ? null :
+                                <div
+                                    style={{
+                                        background: 'red',
+                                        height: '400px',
+                                        width: '400px',
+                                    }}
+                                />
+                            }
+                            <img style={loaded ? {} : { display: 'none' }}
+                                src={images[`${page}`]}
+                                onLoad={() => setLoaded(true)}
+                                alt="img" />
                         </div>
                     </div>
+                </div>
+                <div class="page">
+                    <Stack spacing={2} >
+                        {/* <Typography>Page: {page}</Typography> */}
+                        <Pagination size="large" variant="outlined" count={10} page={page} onChange={handleChange} />
+                    </Stack>
                 </div>
             </div>
         </section>
--- src/Pages/Public.js
+++ src/Pages/Public.js
@@ -20,13 +20,13 @@ const Public = () => {
                     <a href='https://doi.org/10.1063/1.4979635'>https://doi.org/10.1063/1.4979635</a>
                     {/* <h1 class="section-title">2008 <span></span></h1> */}
                     <p> - N. Misra, L. Xu, M.S. Rogers, S.H. Ko, C.P. Grigoropoulos, Pulsed Laser Annealing of Semiconductor Structures for Functional Devices, Physica Status Solidi, vol. 5, p3264, July 2008. SCI </p>
-                    <a>http</a>
+                    <a>https://onlinelibrary.wiley.com/doi/abs/10.1002/pssc.200779506</a>
                     {/* <h1 class="section-title">2007 <span></span></h1> */}
                     <p> - Misra, Nipun; Xu, Li; Pan, Yaoling; Cheung, Nathan; Grigoropoulos, Costas P., “Excimer laser annealing of silicon nanowires”, Applied Physics Letters, vol. 90, p111111, 2007. SCI</p>
-                    <a>http</a>
+                    <a>https://doi.org/10.1063/1.2713774</a>
                     {/* <h1 class="section-title">2006 <span></span></h1> */}
                     <p> - L. Xu, C. Grigoropoulos, T.-J. King, “High Performance Thin Silicon Film Transistors Fabricated By Double Laser Crystallization”, Journal of Applied Physics, vol. 99, p034508, 2006. SCI</p>
-                    <a>http</a>
+                    <a>https://doi.org/10.1063/1.2171807</a>
                     
                 </div>
             </section>
